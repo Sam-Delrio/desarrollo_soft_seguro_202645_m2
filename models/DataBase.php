0e0bdbe611171156823bc1775e613db1c7215715
Sam-Delrio <vinter.delrio@proton.me>
2026-08-07 20:44:55 -0500

    correccion: obtener credenciales de base de datos desde variables de entorno

--- models/DataBase.php
+++ models/DataBase.php
@@ -2,14 +2,16 @@
     class DataBase{
         #  Conexión Local
         public static function connection(){
-            $hostname = "localhost";
-            $port = "3306";
-            $database = "db_inventory";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			return $pdo;
-		}
-	}
+            $hostname = getenv('DB_HOST') ?: "localhost";
+            $port = getenv('DB_PORT') ?: "3306";
+            $database = getenv('DB_NAME') ?: "db_inventory";
+            $username = getenv('DB_USER') ?: "root";
+
+            $password = getenv('MYSQL_SECURE_PASSWORD') !== false ? getenv('MYSQL_SECURE_PASSWORD') : "";
+
+            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        }
+    }
 ?>
